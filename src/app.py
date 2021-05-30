6a62c9a1b1872d289868867531c5091d9bdd54e8
Guillaume MAUCOTEL <guillaume.maucotel@alten.com>
2021-05-30 14:40:06 +0200

    Adding platforms

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import datetime
 from flask import Flask, jsonify, abort, request
 import traceback
 import sys
 
-from src.db import db, Game
+from src.db import db, Game, Platform
 
 app = Flask(__name__)
 app.config.from_object("src.config.Config")
@@ -24,10 +25,21 @@ def get(id):
 @app.route("/set", methods=['POST'])
 def set():
     try:
-        print(request.get_json(force=True))
-        name = request.json["name"]
-        rating = request.json["rating"]
-        game = Game(name,rating)
+        request.get_json(force=True)
+        name     = request.json["name"]
+        # checking the game is not registered
+        if(Game.query.filter(Game.name.ilike(name)) is not None):
+            abort(400,"Game already is database")
+        ratings  = request.json["ratings"]
+        studio   = request.json["studio"]
+        release_date = datetime.date.fromstring(request.json["release_date"])
+        game     = Game(name, ratings, studio)
+        for platform in request.json["platforms"]:
+            existing_platform = Platform.query.filter(Platform.name.ilike(platform)).one_or_none()
+            if(existing_platform is None):
+                game.platforms.append(Platform(platform))
+            else:
+                game.platforms.append(existing_platform)
         db.session.add(game)
         db.session.commit()
     except BaseException:
--- src/db.py
+++ src/db.py
@@ -4,13 +4,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+game_platform = db.Table('game_platform',
+            db.Column('game_id', db.Integer, db.ForeignKey('games.id'), primary_key=True),
+            db.Column('platform_id', db.Integer, db.ForeignKey('platforms.id'), primary_key=True)
+            )
+            
+
 class Game(db.Model):
     __tablename__ = "games"
 
-    id     = db.Column(db.Integer, primary_key=True)
-    name   = db.Column(db.String(128), unique=True, nullable=False)
-    rating = db.Column(db.Integer, default=-1, nullable=False)
+    id           = db.Column(db.Integer, primary_key=True)
+    name         = db.Column(db.String(128), unique=True, nullable=False)
+    studio       = db.Column(db.String(128), unique=False, nullable=False)
+    ratings      = db.Column(db.Integer, nullable=False)
+    release_date = db.Column(db.Date, nullable=False) 
+    platforms = db.relationship('Platform', secondary=game_platform, lazy='subquery', backref=db.backref('games', lazy=True))
 
-    def __init__(self,name,rating):
-        self.name    = name
-        self.rating  = rating
+    def __init__(self, name, ratings, studio, release_date):
+        self.name         = name
+        self.ratings      = ratings
+        self.studio       = studio
+        self.release_date = release_date
+
+class Platform(db.Model):
+    __tablename__ = "platforms"
+
+    id    = db.Column(db.Integer, primary_key=True)
+    name  = db.Column(db.String(128), unique=True, nullable=False)
+
+    def __init__(self,name):
+        self.name = name
